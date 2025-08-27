858688dbd16b770fdcd2252f204bff8ded2a7111
PAOLO <paolo.pena@autonomadeica.edu.pe>
2025-08-27 13:03:55 -0500

    Fin seccion 7

---
+++ src/common/common.module.ts
@@ -0,0 +1,6 @@
+// este modulo se crea desde terminal -----> sirve cpara colocar los pippes personalizados
+// nest g mo common
+import { Module } from '@nestjs/common';
+
+@Module({})
+export class CommonModule {}
